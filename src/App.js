35962fd3586d236903b13139c66f5e5987a8cc40
Dina21 <dinasaginbayeva21@gmail.com>
2023-12-14 00:28:34 +0600

    added counter functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './index.scss';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count+1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={decrease}>- Минус</button>
+        <button className="plus" onClick={increase}>Плюс +</button>
       </div>
     </div>
   );
